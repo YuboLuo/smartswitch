5fe3133d96dcb80593cfaba6b404a685e8da82e7
Yubo Luo <yuboluo@Yubos-MacBook-Pro.local>
2022-02-21 13:46:35 -0600

    clustering done

--- decomposition/taskgroup.py
+++ decomposition/taskgroup.py
@@ -8,6 +8,7 @@ from scipy import stats
 from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 
 def dataload_mnist(chosenType):
@@ -66,10 +67,6 @@ def model_train(train = True, chosenType = 0):
         model.add(layers.Dense(32, activation='relu'))
         model.add(layers.Dense(64, activation='relu'))
         model.add(layers.Dense(2))
-        model.add(layers.Flatten())
-        model.add(layers.Dense(32, activation='relu'))
-        model.add(layers.Dense(64, activation='relu'))
-        model.add(layers.Dense(2))
         # model.summary()
 
 
@@ -84,12 +81,14 @@ def model_train(train = True, chosenType = 0):
     return model
 
 
-def RDM_Calc(K, chosenType = 0, train = False):
+def RDM_Calc(K, chosenType = 0, train = False, getFuncNumber = False):
+
     '''
     calculate the Representation Dissimilarity Matrix (RDM) for each possible branch out point
     this function is for one task, you need to run this function repeatedly for all tasks
     :param K: the number of images to use for calculation, better to use 50 or more
     :param chosenType: the class type you use as 1 for this task
+    :param getFuncNumber: if this param is True, we just return the number of func, a.k.a the number of branch out points
     :return: RDM for this task
     '''
 
@@ -104,11 +103,13 @@ def RDM_Calc(K, chosenType = 0, train = False):
     setup functions to read outputs of intermediate layers
     those functions are related to specific model architecture, so design here accordingly
     '''
-    func0 = F.function([model.layers[0].input], [model.layers[0].output])
-    func1 = F.function([model.layers[0].input], [model.layers[2].output])
-    func2 = F.function([model.layers[0].input], [model.layers[4].output])
-    func3 = F.function([model.layers[0].input], [model.layers[5].output])
-    funcList = [func0, func1, func2, func3]
+    func0 = F.function([model.layers[0].input], [model.layers[1].output])
+    func1 = F.function([model.layers[0].input], [model.layers[4].output])
+    func2 = F.function([model.layers[0].input], [model.layers[5].output])
+    funcList = [func0, func1, func2]
+
+    if getFuncNumber:
+        return len(funcList)
 
     ### read outputs from intermediate layers of the model using K images
     K = len(images)  # K images were used
@@ -136,17 +137,17 @@ def RDM_Calc(K, chosenType = 0, train = False):
 
 def RSM_Calc(K):
     '''
-    calculate the task-wise Representation Similarity Matrix (RSM)
+    calculate the task-wise Representation Similarity Matrix (RSM) - the paper calls it task affinity tensor A
     :param K: the number of images to use for RDM calculation
     :return:
     '''
 
-    ### some parameters
+    # some parameters
     T = 10 # number of tasks, mnist dataset has 10 classes and we divide it into 10 individual tasks
-    D = 4 # number of division points where you possibly branch out
+    D = RDM_Calc(1, getFuncNumber = True) # number of division points where you possibly branch out
     RSM = np.zeros((D, T, T))
 
-    ### calculate RDM for each task
+    # calculate RDM for each task
     RDM = [RDM_Calc(K, chosenType = t) for t in range(T)]
 
     ###
@@ -154,24 +155,167 @@ def RSM_Calc(K):
         for i in range(T):
             for j in range(T):
 
-                ### extract RDM of the d_th division point for task_i and task_j
+                # extract RDM of the d_th division point for task_i and task_j
                 m1, m2 = RDM[i][d], RDM[j][d]
 
-                ### extract the upper triangle of the matrix and flatten them into a list
+                # extract the upper triangle of the matrix and flatten them into a list
                 p1 = [elem for ii, row in enumerate(m1) for jj, elem in enumerate(row) if ii < jj]
                 p2 = [elem for ii, row in enumerate(m2) for jj, elem in enumerate(row) if ii < jj]
 
-                ### calcualte the Spearman’s correlation coefficient for task_i and task_j at the d_th division point
+                # calculate the Spearman’s correlation coefficient for task_i and task_j at the d_th division point
                 RSM[d][i][j] = stats.spearmanr(p1, p2).correlation
     return RSM
 
-rsm = RSM_Calc(50)
+
+
+def partition(collection):
+    '''
+    Bell Number reference: https://en.m.wikipedia.org/wiki/Bell_number
+    implementation reference: https://stackoverflow.com/questions/19368375/set-partitions-in-python
+    return all possible partitions of a list of unique numbers
+    "A partition of a set X is a set of non-empty subsets of X such that
+    every element x in X is in exactly one of these subsets"
+    :param collection: a list of unique numbers
+    :return: all possible partitions
+    '''
+    if len(collection) == 1:
+        yield [ collection ]
+        return
+
+    first = collection[0]
+    for smaller in partition(collection[1:]):
+        # insert `first` in each of the subpartition's subsets
+        for n, subset in enumerate(smaller):
+            yield smaller[:n] + [[ first ] + subset]  + smaller[n+1:]
+        # put `first` in its own subset
+        yield [ [ first ] ] + smaller
+
+
+# we save and reload RSM info
+# rsm = RSM_Calc(50)
+# np.save('rsm.npy', rsm)
+# RSM = np.load('rsm.npy')
+
+
+
+def ScoreCalc(clusters, depth, RSM):
+    '''
+    calculate the task dissimilarity score for a given division point
+    :param clusters: the input cluster list, e.g. for [[1,2],[3,4]], [1,2] is a cluster, [3,4] is a cluster
+    :param depth: the division point
+    :param RSM: pre-computed Representation Similarity Matrix - the paper calls it task affinity tensor A
+    :return: the averaged maximum distance between the dissimilarity scores of the elements in every cluster
+    '''
+
+
+    tobeAveraged = 0
+    for cluster in clusters:
+        n = len(cluster)
+
+        if n <= 1:
+            tobeAveraged += 0 # the dissimilarity between a task and itself is 0
+        else:
+            '''
+            calculate all pair-wise dissimilarity scores for all elements in this cluster
+            then only keep the largest score and add it to the tobeAveraged variable
+            '''
+            max_score = float('-inf')
+            for i in range(n - 1):
+                for j in range(i + 1, n):
+
+                    # 1 - RSM(d, i, j) is the dissimilarity score between task_i and task_j at division point d
+                    if 1 - RSM[depth][cluster[i]][cluster[j]] > max_score:
+                        max_score = 1 - RSM[depth][cluster[i]][cluster[j]]
+
+            tobeAveraged += max_score
+
+    return tobeAveraged / len(clusters)
+
+
+def clustering(RSM):
+    '''
+
+    '''
+    tasks = list(range(5))
+    queue = [] # to save all possible trees, and their score and model size
+
+    # get the weight size of each layer
+    model = model_train(train=False)
+    var = model.trainable_variables
+    sizeByLayer = [F.count_params(i) + F.count_params(j) for i, j in zip(var[0::2], var[1::2])]
+    sizeByBlock = [sizeByLayer[0] + sizeByLayer[1]] + sizeByLayer[2:]
+
+    for clusters0 in partition(tasks):
+
+        ''' Variable Name Explanation 
+        clustersX, depthX, scoreX: 'X' means the Xth branch out point
+        clustersX: means the branched-out result at Xth branch out point
+        scoreX: means the dissimilarity score for clustersX
+        '''
+
+        depth0, depth1 = 0, 1
+        score0 = ScoreCalc(clusters0, depth0, RSM)
+
+        # print(clusters0)
+        # print('--->')
+        for clusters1 in itertools.product(*[partition(cluster) for cluster in clusters0]):
+            '''
+            we use itertools.product to output all combinations of possible subtrees
+            please refer to: https://stackoverflow.com/questions/12935194/permutations-between-two-lists-of-unequal-length
+            to see understand itertools.product(*[ ]) works
+            '''
+
+            # strip the out layer brackets
+            res = []
+            for l in clusters1:
+                res += l
+            clusters1 = res
+            # print(res)
+
+            score1 = ScoreCalc(clusters1, depth1, RSM)
+
+            '''
+            our paper only has three layers of branch out points, for the final one, it is always branched into single tasks
+            so, we do not need to continue the processing of clusters2/score2
+            clusters2 will all be single tasks, e.g. clusters2 should always be [[0],[1],[2],[3],[4]] if 5 tasks in total. 
+            then score2 will always be 0; 
+            '''
+
+            # summarize all info and pack them into the queue
+            score = score0 + score1  # total dissimilarity score of all branch out points, score2 is omitted as it is 0
+            model_size = sizeByBlock[0] + len(clusters0) * sizeByBlock[1] \
+                         + len(clusters1) * sizeByBlock[2] \
+                         + len(tasks) * sizeByBlock[3] # total model size this tree requires
+            tree = [clusters0, '--->' ,clusters1]  # decomposition details
+
+            queue.append([score, model_size, tree])
+
+        # print('\n\n')
+
+    if len(tasks) <= 9:  # if more than 10, queue has 16733779 elements, too slow to be sorted
+        queue.sort(key = lambda x:(x[1], x[0]))
+
+    return queue
+
+def plotQueue(queue):
+    dic = defaultdict(list)
+    for q in queue:
+        dic[q[1]].append(q[0])
+
+    for idx, key in enumerate(dic.keys()):
+        plt.boxplot(dic[key], positions=[idx], showfliers=False)  # do not plot outliers
+
+    plt.show()
+
+
+RSM = np.load('rsm.npy')
+queue = clustering(RSM)
+plotQueue(queue)
 
 
 
 
 
-# ans = RDM_Calc(50, chosenType = 9, train = True)
 
 
 
