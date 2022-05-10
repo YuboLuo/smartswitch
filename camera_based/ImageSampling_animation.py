681ba2d85aad8d1d062d9c136566df1a311c4eee
Yubo Luo <yuboluo@yubos-mbp.wireless-psk.unc.edu>
2022-05-10 16:27:56 -0400

    hm01b0 reg file

--- camera_based/ImageSampling_animation.py
+++ camera_based/ImageSampling_animation.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import serial
+import cv2
 
-ser = serial.Serial("/dev/cu.usbmodem11401",9600, timeout = 1)
-frameLength, frameWidth = 64, 64
+ser = serial.Serial("/dev/cu.usbmodem1101",9600, timeout = 1)
+frameLength, frameWidth = 96, 96
 frameSize = frameLength * frameWidth
+identifier = [0x55, 0xAA]
 
 
 # plot
@@ -19,13 +21,19 @@ ax.imshow(image, cmap='gray', animated=True)
 def get_image():
 
     data = ser.read(int(frameSize * 2.1))
+    # ser.reset_input_buffer()
+    # ser.flushOutput()
+    # while 1:
+    #     data = ser.read_until(b'\x55\xAA')
+    #     if len(data) == frameSize + 2:
+    #         break
+
     idx = 0
     for idx in range(len(data) - 1):
-        if data[idx] == 0x55 and data[idx + 1] == 0xAA:  # image starts with this 2byte identifier
+        if data[idx] == identifier[0] and data[idx + 1] == identifier[1]:  # image starts with this 2byte identifier
             idx += 2  # move over the identifier
             break
 
-    print(idx)
     frame = data[idx:idx + frameSize]  # get the image
     frame = np.frombuffer(frame, dtype=np.uint8)  # convert into numpy array
     image = np.reshape(frame, (frameLength, frameWidth))  # reshape
@@ -34,39 +42,16 @@ def get_image():
 
 def updatefig(*args):
     image = get_image()
-    print(image[30,:10])
+    cv2.imwrite("image.pgm", image) # save image to local
     ax.imshow(image,cmap='gray')
     return ax,
 
-anim = animation.FuncAnimation(fig, updatefig, interval=1, blit=True)
+anim = animation.FuncAnimation(fig, updatefig, interval=10, blit=True)
 plt.show()
 
-# while 1:
 
 
 
-
-
-
-
-# state, read, result, startbyte = 0, 0, 0, 0
-# while 1:
-#     if read == 0:
-#         startbyte = ser.read()
-#         if (startbyte == 0x55):
-#             state = 1
-#
-#         if (startbyte == 0xAA and state == 1):
-#             read = 1
-#
-#         if (startbyte == 0xBB and state == 1):
-#             result = 1
-#     if result == 1:
-#         ser.read(2)
-#
-#     if read == 1:
-#         res = ser.read(frameSize)
-#         read = 0
 
 
 
